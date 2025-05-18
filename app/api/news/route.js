c5ae1266d84cd3f895e1f211b3780f8909888eb2
Prabhleen Singh <workprabhsingh1@gmail.com>
2025-05-18 14:01:25 +0530

    Update route.js

--- app/api/news/route.js
+++ app/api/news/route.js
@@ -6,7 +6,7 @@ export async function GET(request) {
   console.log(query);
  var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var mm = String(today.getMonth() + 1).padStart(2, '0');
 var yyyy = today.getFullYear();
 
 today = yyyy + '-' + mm + '-' + dd;
